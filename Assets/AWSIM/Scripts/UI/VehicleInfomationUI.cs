2458e564b0dbb55a59a0e4337636e86241aeb8df
tyongingo <yuuki.k5963@icloud.com>
2024-10-11 19:03:23 +0900

    Add UI, Smoothing steering

--- Assets/AWSIM/Scripts/UI/VehicleInfomationUI.cs
+++ Assets/AWSIM/Scripts/UI/VehicleInfomationUI.cs
@@ -13,17 +13,23 @@ namespace AWSIM
         [SerializeField] public Vehicle vehicle;
         [SerializeField] Text speedText;
         [SerializeField] Text gearText;
+        [SerializeField] Text accelerationText; // 加速度表示用のTextフィールド
+        [SerializeField] Text steerAngleText; // ステアアングル表示用のTextフィールド
 
         void Update()
         {
             if (!vehicle) {
                 speedText.text = "";
                 gearText.text = "";
+                accelerationText.text = ""; // 加速度のテキストをクリア
+                steerAngleText.text = ""; // ステアアングルのテキストをクリア
                 return;
             }
 
             speedText.text = "" + Mathf.Floor(vehicle.Speed * 3.6f);
             gearText.text = "" + GetShiftString(vehicle.AutomaticShift);
+            accelerationText.text = "" + Mathf.Floor(vehicle.LocalAcceleration.z * 100) / 100; // 前方方向への加速度を表示
+            steerAngleText.text = "" + Mathf.Floor(vehicle.SteerAngle * 100) / 100; // ステアアングルを表示
 
             static string GetShiftString(Vehicle.Shift shift)
             {
